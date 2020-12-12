f7e4b05fe75e1d7ec4402a8747e909c6d0f775fe
Christopher Enriquez <topher.carpediem@gmail.com>
2020-12-12 16:30:32 +0800

    Initial commit

---
+++ src/AutomatedCertificateSender/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Extensions.Hosting;
+
+namespace automated_certificate_sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) => 
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices(services => {
+
+                    
+                });
+    }
+}
